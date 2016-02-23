b5b88116485138bb05b76317bc9c597282c30650
Sunny Sompalli <sunnysompalli@gmail.com>
2016-02-22 22:29:15 -0500

    Compiles, but doesn't work correctly

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,20 +1,86 @@
-public class KnightsBoard{
+public class KnightBoard{
     private int[][] knightboard;
 
 
-    public KnightsBoard(int row, int col){
-	col += 4; //I made these two += 4 because I am going to use the method 
-	row += 4; //of solving this that adds two spaces on each side.
+    public KnightBoard(int row, int col){
 	knightboard = new int[row][col];
     }
 
-    public KnightsBoard(int side){
+    public KnightBoard(int side){
 	this(side, side);
     }
 
-    public KnightsBoard(){
+    public KnightBoard(){
 	this(4);
     }
 
+    public boolean solve(){
+	return solveH(0, 0, 0);
+    }
+
+    private boolean solveH(int row, int col, int n){
+	if(row < 0){
+	    return false;
+	}
+	if(col < 0){
+	    return false;
+	}
+	if(row >= knightboard.length){
+	    return false;
+	}
+	if(col >= knightboard[0].length){
+	    return false;
+	}  
+	if(knightboard[row][col] != -1){
+	    return false;
+	}
+	if(n == (knightboard.length * knightboard[0].length - 1)){
+	    knightboard[row][col] = n;
+	    return true;
+	}
+	else{
+	    knightboard[row][col] = n;
+	    if(
+	       solveH(row + 1, col + 2, n + 1) ||
+	       solveH(row + 1, col - 2, n + 1) ||
+	       solveH(row + 2, col + 1, n + 1) ||
+	       solveH(row + 2, col - 1, n + 1) ||
+	       solveH(row - 1, col + 2, n + 1) ||
+	       solveH(row - 1, col - 2, n + 1) ||
+	       solveH(row - 2, col + 1, n + 1) ||
+	       solveH(row - 2, col - 1, n + 1)
+	       ){
+		printSolution();
+		return true;
+	    }
+	    else{
+		knightboard[row][col] = -1;
+		return false;
+	    }
+	}
+    }
+
+    public void printSolution(){
+	String ans = "";
+	for(int i = 0; i < knightboard.length; i++){
+	    for(int j = 0; j < knightboard.length; j ++){
+		if(knightboard[i][j] < 10){
+		    ans += "_";
+		}
+		ans += knightboard[i][j] + " ";	  
+	    }
+	    ans += "\n";
+	}
+	System.out.println(ans);
+    }
+
+
+
+    //Tests
+    public static void main(String[]args){
+	KnightBoard A = new KnightBoard(5);
+	System.out.println(A.solve());
+	A.printSolution();
+    }
 
 }
