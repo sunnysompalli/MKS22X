422d9d30f1ce59a456da60fb10c9d30544637079
Sunny Sompalli <sunnysompalli@gmail.com>
2016-02-24 00:06:07 -0500

    Wrote stomp method

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -18,6 +18,25 @@ public class Bronze{
     }
 
     public void stomp(int R_s, int C_s, int D_s){
+	R_s -= 1;
+	C_s -= 1;
+	int max = pasture[R_s][C_s];
+	for(int i = R_s; i < Math.min(pasture.length - 1, R_s + 3); i++){
+	    for(int j = C_s; j < Math.min(pasture[0].length - 1, C_s + 3); j++){
+		max = Math.max(max, pasture[i][j]);
+	    }
+	}
+	int new = max - D_s;
+	for(int i = R_s; i < Math.min(pasture.length - 1, R_s + 3); i++){
+	    for(int j = C_s; j < Math.min(pasture[0].length - 1, C_s + 3); j++){
+		if(pasture[i][j] > new){
+		    pasture[i][j] = new;
+		}
+	    }
+	}
+    }
+
+    public void final(int E){
 	
     }
 
