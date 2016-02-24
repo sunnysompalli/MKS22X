a3e9281291eebddd50cfc1b9a046fed651bc8d47
Sunny Sompalli <sunnysompalli@gmail.com>
2016-02-23 23:50:04 -0500

    Added another constructor

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -4,7 +4,22 @@ import java.io.*;
 public class Bronze{
     private int[][] pasture;
 
-    public makeLake(int row, int col, 
+    public makeLake(int R, int C){
+	pasture = new int[R][C];
+    }
+
+    public makeLake(int[][] ary){
+	pasture = new int[ary.length][ary[0].length];
+	for(int i = 0; i < ary.length; i++){
+	    for(int j = 0; j < ary[0].length; j++){
+		pasture[i][j] = ary[i][j];
+	    }
+	}
+    }
+
+    public void stomp(int R_s, int C_s, int D_s){
+	
+    }
 
 
 }
