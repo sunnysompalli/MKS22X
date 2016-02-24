ccb7ad8444db590745e31b1a8af1c625941ae475
Sunny Sompalli <sunnysompalli@gmail.com>
2016-02-24 00:32:12 -0500

    Finished, but need to test

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -4,19 +4,6 @@ import java.io.*;
 public class Bronze{
     private int[][] pasture;
 
-    public makeLake(int R, int C){
-	pasture = new int[R][C];
-    }
-
-    public makeLake(int[][] ary){
-	pasture = new int[ary.length][ary[0].length];
-	for(int i = 0; i < ary.length; i++){
-	    for(int j = 0; j < ary[0].length; j++){
-		pasture[i][j] = ary[i][j];
-	    }
-	}
-    }
-
     public void stomp(int R_s, int C_s, int D_s){
 	R_s -= 1;
 	C_s -= 1;
@@ -48,6 +35,36 @@ public class Bronze{
 	    }
 	}
 	return (sum * 72 * 72) + ",6,Sompalli,Sunny";
+    }
+
+
+    public static void main(String[args]){
+	try{
+	    File f = new File("makelake.in");
+	    Scanner in = new Scanner(f);
+	    int R = f.nextInt();
+	    int C = f.nextInt();
+	    int E = f.nextInt();
+	    int N = f.nextInt();
+	    pasture = new int[R][C];
+	    for(int i = 0; i < R; i++){
+		for(int j = 0; j < C; j++){
+		    pasture[i][j] = f.nextInt();
+		}
+	    }
+
+	    while(N > 0){
+		stomp(f.nextInt(), f.nextInt(), f.nextInt());
+		N -= 1;
+	    }
+
+	    System.out.println(final(E));
+
+	}
+	catch (FileNotFoundException e){
+	    System.out.println("File not found");
+	}
+
     }
 
 
