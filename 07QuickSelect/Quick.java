bc6d2f7e3aa35973cb309f60386238cd9be119da
Sunny Sompalli <sunnysompalli@gmail.com>
2016-03-08 21:23:54 -0500

    Wrote partition

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -1,12 +1,36 @@
 public class Quick{
 
     private static int partition(int[]data, int left, int right){
-	int ans = ((int)(Math.random() * (right - left))) + left;
-	boolean haveAns = false;
-	while (haveAns == false){
-	    
+	int initial = ((int)(Math.random() * (right - left))) + left;
+	int holder;
+	int rightend = right;
+	data[initial] = num;
+	data[initial] = data[right];
+	data[right] = num;
+	right--;
+	while (left < right){
+	    if (data[left] > num){
+		holder = data[right];
+		data[right] = data[left];
+		data[left] = holder;
+		right--;
+	    } else {
+		left++;
+	    }
+	}
+	if (num < data[left]){
+	    holder = data[left];
+	    data[left] = num;
+	    data[rightend] = holder;
+	    return left;
+	} else {
+	    holder = data[left + 1];
+	    data[left + 1] = num;
+	    data[rightend] = holder;
+	    return left + 1;
 	}
     }
+
 
     
 }
---
+++ 07QuickSelect/Quick.java~
@@ -0,0 +1,12 @@
+public class Quick{
+
+    private static int partition(int[]data, int left, int right){
+	int ans = ((int)(Math.random() * (right - left))) + left;
+	boolean haveAns = false;
+	while (haveAns == false){
+	    
+	}
+    }
+
+    
+}
