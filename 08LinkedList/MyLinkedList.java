7f3eca4634ba9e114d44419473ea2250f8b7bf22
Sunny Sompalli <sunnysompalli@gmail.com>
2016-03-16 12:01:20 -0400

    Copied Mr. K's code

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,33 +1,74 @@
 public class MyLinkedList{
-    private LNode start;
-    private int size;
-    
-    private class LNode{
-	private int data;
-	private LNode next;
 
-	private LNode(){
-	   
+    private class LNode{
+	//instance variables
+	int value;
+	LNode next;
+	
+	public LNode(int v){
+	    value = v;
 	}
-	private LNode(int d){
-	    data = d;
-	    next = null;
+
+	public int getValue(){
+	    return value;
 	}
-	private LNode(int d; LNode n){
-	    data = d;
+	public LNode getNext(){
+	    return next;
+	}
+	public void setValue(int v){
+	    value = v;
+	}
+	public void setNext(LNode n){
 	    next = n;
 	}
+    }
 
-	private int get(){
-	    return data;
+    //instance Variables
+    LNode head;
+    int size;
+    //methods
+    
+    public boolean add(int value){
+	if(head == null){
+	    head = new LNode(value);
+	}else{
+	    LNode p = head;			
+	    while(p.getNext()!=null){
+		p = p.getNext();
+	    }
+	    p.setNext(new LNode(value));
 	}
-
+	size+=1;
+	return true;
+    }
+ 
+    public String toString(){
+	String ans = "[";
+	LNode p = head;
+	while(p != null){
+	    ans += p.getValue();
+	    if(p.getNext()!= null){
+		ans+=", ";
+	    }
+	    p = p.getNext();
+	}
+	return ans+"]";
     }
 
     public int size(){
 	return size;
     }
-
-    
-
+   
+    public static void main(String[]args){
+	MyLinkedList m = new MyLinkedList();
+	int i = 0;
+	while(i < 100){
+	    m.add(i);
+	    i++;
+	}
+	m.add(54);
+	m.add(-10);
+	m.add(47);
+	System.out.println(m);
+    }
 }
