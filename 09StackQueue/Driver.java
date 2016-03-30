85ecd49cc45f44c7c0c8a976b0d90132f61db75d
Sunny Sompalli <sunnysompalli@gmail.com>
2016-03-30 12:16:33 -0400

    Started testing

---
+++ 09StackQueue/Driver.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Driver{
+    
+    public static void main(String[]args){
+	MyStack<String> ms = new MyStack<String>();
+	Stack<String> s = new Stack<String>();
+	MyQueue<String> mq = new MyQueue<String>();
+	Queue<String> q = new Queue<String>();
+	
+    }
+
+
+}
