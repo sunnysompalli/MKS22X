2dad44ae5557b023a0977e85c32db1a26a76b012
Sunny Sompalli <sunnysompalli@gmail.com>
2016-04-01 12:32:32 -0400

    Started toString method

--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -1,10 +1,19 @@
 public class MyDeque<T>{
-    private T[] ary;
+    private Object[] ary;
     private int size;
     private int start;
     private int end;
 
+    /*
+    public String toString(){
+	String ans = "";
+	ans += "[";
+	if (start > end){
+	    int n = size - start;
+	}
+	return ans;
+    }
+    */
     
-
-
+    
 }
