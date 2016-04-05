827a6e36deceab63f2746ea73ce4e42e2f2df6b8
Sunny Sompalli <sunnysompalli@gmail.com>
2016-04-05 13:58:28 -0400

    Wrote more methods

--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -1,8 +1,69 @@
+import java.util.*;
 public class MyDeque<T>{
-    private Object[] ary;
+    private T[] ary;
     private int size;
     private int start;
     private int end;
+
+    @SuppressWarnings("unchecked")	    
+    public MyDeque(){
+	ary = (T[]) new Object[10];
+    }
+
+    @SuppressWarnings("unchecked")	    
+    private void grow(){
+	T[] newary = (T[]) new Object[ary.length * 2];
+	for(int i = 0; i < ary.length; i++){
+	    newary[i] = ary[start];
+	    start++;
+	    if(start == ary.length){
+		start = 0;
+	    }
+	}
+	ary = newary;
+	start = 0;
+	end = ary.length - 1;
+    }
+
+    public void addFirst(T value){
+	if (size == ary.length){
+	    grow();
+	}
+	if (size == 0){
+	    ary[0] = value;
+	    start = 0;
+	    end = 0;
+	}
+	else if (start == 0){
+	    ary[ary.length - 1] = value;
+	    start = ary.length - 1;
+	} else {
+	    ary[start - 1] = value;
+	    start--;
+	}
+	size++;
+    }
+
+    public void addLast(T value){
+	if (size == ary.length){
+	    grow();
+	}
+	if (size == 0){
+	    ary[0] = value;
+	    start = 0;
+	    end = 0;
+	}
+	else if (end == ary.length - 1){
+	    ary[0] = value;
+	    end = 0;
+	} else {
+	    ary[end + 1] = value;
+	    end ++;
+	}
+	size++;
+    }
+
+    
 
     /*
     public String toString(){
