f7a7093dfa843db7f4ed26093c64349b266a51f4
Sunny Sompalli <sunnysompalli@gmail.com>
2016-05-05 12:09:37 -0400

    Made file and copied in heap methods

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class MyHeap<T extends Comparable<T>>{
+   private int size;
+   private T[] data;
+
+   public MyHeap(){}
+   public MyHeap(T[] array){}
+   private void pushDown(int k){}
+   private void pushUp(int k){}
+   private void heapify(){}
+   public T delete(){}
+   public void add(T x){}
+   private void doubleSize(){}
+   public String toString(){}
+
+   //do this last
+   public MyHeap(boolean isMax){}
+   public MyHeap(T[] array, boolean isMax){}
+}
